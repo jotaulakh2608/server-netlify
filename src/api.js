f79d907946c600a31ee03fb8bf7c3cac79b19e94
jotaulakh2608 <rattanjotsingh30@gmail.casgit config -- global user.email rattanjotsingh30@gmail.com>
2022-11-23 13:08:45 +0530

    change

---
+++ src/api.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express()
+const cors = require('cors');
+const dataApi = require('../data.json');
+const ServerlessHttp = require('serverless-http');
+app.use(cors())
+
+const router = express.Router()
+
+router.get("/",(req,res)=>{
+    res.send('helo')
+})
+
+router.get('/data', (req, res)=>{
+    res.send(dataApi)
+})
+
+app.use('/.netlify/functions/api', router)
+
+module.exports.handler = ServerlessHttp(app)
